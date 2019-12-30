abaf07399a90a0878acad6c5357221203625d7bc
Unknown <clarika.91@gmail.com>
2019-12-30 16:57:54 +0100

    routage vers le client ameliore

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,27 +1,17 @@
-//import * as fs from 'fs'; //
-//import { renderModuleFactory } from '@angular/platform-server';
-//import { AppServerModuleNgFactory } from './aot/src/app/app.server.module.ngfactory';
+/* -- Create a server to serve the angular app files to a client -- */
+
 const portNb : number = 3000;
 
-let express = require('express');
-let app = express();
-app.use(express.static('./clientCode/dist/dlk-app'))
- 
-/** const ngEngine = (filePath, options, callback) => { //
-    const file = fs.readFileSync(filePath).toString();
-    renderModuleFactory(AppServerModuleNgFactory, {
-      document: file,
-      url: options.req.url
-    })
-    .then(string => {
-      callback(null, string);
-    });
-  };
-   
-  app.engine('html', ngEngine);
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.get('*', (request, response) => { //
-    response.render('index', { req: request });
-  });   */
+// Load all the files in dlk-app forder in order to sent them to a client :
+app.use(express.static('./clientCode/dist/dlk-app')) ;
+// Rooting : for any client request, send index.html file ie angular is going to manage the rooting :
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../clientCode/dist/dlk-app/index.html'));
+});
 
+// Start the server :
 app.listen(portNb);
